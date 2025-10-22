a62c540a884bf4748c6205e63d52110cc56c14da
Pramit Lama <kyriositsolutions@gmail.com>
2025-10-22 13:49:07 +1100

    CORS Restrictions added on backend

--- server/index.js
+++ server/index.js
@@ -2,7 +2,13 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: [
+        'http://localhost:3000',
+        "https://todo-app-fullstack-frontend.onrender.com",
+        "https://todo-app-fullstack-live.onrender.com"
+    ]
+}));
 app.use(express.json());
 
 let todos = [];
